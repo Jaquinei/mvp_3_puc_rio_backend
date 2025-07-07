1be0391ea9fdbb4e756e52d00856a1edf7c2b7c5
Jaquinei de Oliveira <jaquinei@gmail.com>
2025-07-06 21:19:28 -0300

    feat: read API_EXTERNA_DATABASE_ID and API_EXTERNA_TOKEN from environment variable

--- server.mjs
+++ server.mjs
@@ -1,21 +1,23 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 app.use(cors()); // Allow all origins (for development)
 app.use(express.json());
 
+const NOTION_DATABASE_ID = process.env.API_EXTERNA_DATABASE_ID;
+const NOTION_TOKEN = process.env.API_EXTERNA_TOKEN;
+
 app.post('/notion', async (req, res) => {
   console.log("/notion: Received request to endpoint");
 
-  const { token, database } = req.body;
-  if (!token || !database) {
+  if (!NOTION_DATABASE_ID || !NOTION_TOKEN) {
     console.log('/notion: Error: TOKEN and DATABASE info missing');
     return res.status(400).json({ error: 'TOKEN and DATABASE info missing' });
   }
-
-  const notion_api_url = `https://api.notion.com/v1/databases/${database}/query`;
+  const notion_api_url = `https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`;
 
   console.log('/notion: Querying Notion API URL:', notion_api_url);
 
@@ -23,7 +25,7 @@ app.post('/notion', async (req, res) => {
     const response = await fetch(notion_api_url, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`,
+        'Authorization': `Bearer ${NOTION_TOKEN}`,
         'Notion-Version': '2021-05-13',
         'Content-Type': 'application/json'
       }
@@ -43,7 +45,14 @@ app.post('/notion', async (req, res) => {
   }
 });
 
+if (!NOTION_DATABASE_ID || !NOTION_TOKEN) {
+  console.log('[SERVER] (warning) TOKEN and DATABASE info is missing!');
+  console.log('[SERVER] (warning) Requests will fail.');
+  console.log('[SERVER] (info) Ensure to set API_EXTERNA_DATABASE_ID and API_EXTERNA_TOKEN environment variables.');
+  console.log('\n');
+}
+
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Backend server running on http://localhost:${PORT}`);
+  console.log(`[SERVER] Backend server running on http://localhost:${PORT}`);
 });
